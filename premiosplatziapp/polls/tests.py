e177bfbbacbad1d5196162e91d824915011332f0
Andrea Soriano <aasorian@espol.edu.ec>
2022-03-18 16:08:22 -0500

    Realizando tests

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -75,3 +75,29 @@ class QuestionIndexViewTests(TestCase):
         past_question2 = create_question(question_text="Past question2", days=-40)
         response = self.client.get(reverse("polls:index"))
         self.assertQuerysetEqual(response.context["latest_question_list"],[past_question1,past_question2])
+
+    def test_two_future_questions(self):
+        """The questions index page are not displayed multiple future questions"""
+        future_question1 = create_question(question_text="Past question1", days=30)
+        future_question2 = create_question(question_text="Past question2", days=40)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"],[])
+
+class questionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """The detail view of a question with a pub_date in the future returns
+        a 404 error not found"""
+        future_question = create_question(question_text="Future question",days=30)
+        url = reverse("polls:detail",args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,404)
+        
+    def test_past_question(self):
+        """The detail view of a question with a pub_date in the past displays
+        the question's text"""
+        past_question = create_question(question_text="Past question", days=-30)
+        url = reverse("polls:detail",args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response.status_code,past_question.question_text)
+        
